2adef1ccd37dc67f34ef1e5dbbf416e8fa410492
GaspardDandria <gaspard.dandria@gmail.com>
2021-01-12 18:48:40 +0100

    Final

--- exo_16.rb
+++ exo_16.rb
@@ -1,1 +1,12 @@
-puts "Bonjour, monde !"
+puts "Choisis un nombre"
+etages = gets.chomp.to_i
+t=5
+i = 0
+i.upto(etages) do
+  i.times do
+    print '#'
+  end
+  print "\n"
+  etages -= 1
+  i += 1
+end
--- exo_17.rb
+++ exo_17.rb
@@ -1,1 +1,17 @@
-puts "Bonjour, monde !"
+puts "Choisis un nombre"
+etages = gets.chomp.to_i
+t=5
+i = 0
+
+i.upto(etages) do
+  etages.times do
+  print ' '
+  end
+
+  (2*i + 1).times do
+  print '#'
+  end
+  print "\n"
+  etages -= 1
+  i += 1
+end
